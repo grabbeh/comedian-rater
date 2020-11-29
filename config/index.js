d63d6378ac861245efb4570663240ab6234ec527
Michael <mbg@outlook.com>
2020-11-29 23:12:35 +0000

    Site

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const server = dev
   ? 'http://localhost:3000/api'
-  : 'http://localhost:3000/api'
+  : 'https://https://comedian-rater.vercel.app/api'
