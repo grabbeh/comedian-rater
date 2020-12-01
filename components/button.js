1db3097f0fecc47b855c5b294487439429cf8c52
Michael <mbg@outlook.com>
2020-12-01 09:08:28 +0000

    Links

--- components/button.js
+++ components/button.js
@@ -1,7 +1,7 @@
 const Button = ({ children }) => (
   <button
     type='submit'
-    className='bg-blue-500 rounded text-white text-xl px-5 py-3 font-bold flex-shrink-0'
+    className='bg-blue-500 rounded text-white text-2xl px-7 py-3 font-bold flex-shrink-0'
   >
     {children}
   </button>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
 import Button from '../components/button'
 import Container from '../components/container'
 import Header from '../components/header'
+import Link from 'next/link'
 
 const Index = () => (
   <Container>
     <div className='space-y-10'>
       <Header>Comedian rater</Header>
-      <Button>Click me</Button>
+      <div className='flex justify-between'>
+        <Button>
+          <Link href='/random'>
+            <a>Rate</a>
+          </Link>
+        </Button>
+        <Button>
+          <Link href='/results'>
+            <a>Results</a>
+          </Link>
+        </Button>
+      </div>
+
+      <div className='h-10 mt-4 bg-green-500'></div>
+      <div className='h-10 mt-4 bg-blue-500'></div>
+      <div className='h-10 mt-4 bg-purple-500'></div>
+      <div className='h-10 mt-4 bg-red-500'></div>
+      <div className='h-10 mt-4 bg-yellow-500'></div>
     </div>
   </Container>
 )
